7c8b4335d6d768814ea618d56728e1faf91a74db
alanmdsan <alanmdsan@gmail.com>
2024-02-12 11:53:55 -0300

    setting reducers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import Home from './pages/Home'
 import Singlepost from './pages/SinglePost'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" exact component={() => <Home />} />
-          <Route path="/:id" exact component={() => <Singlepost />} />
-        </Routes>
-      </Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" exact component={() => <Home />} />
+        <Route path="/:id" exact component={() => <Singlepost />} />
+      </Routes>
     </div>
   )
 }
